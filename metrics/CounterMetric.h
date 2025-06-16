9da8ff3b6c946d46b9b7ac4719cb47a373faeddb
Vladislav <vladislav-bodnya@mail.ru>
2025-06-16 22:44:18 +0300

    Corrected metrics

--- metrics/CounterMetric.h
+++ metrics/CounterMetric.h
@@ -8,12 +8,12 @@ class CounterMetric : public Metric {
 public:
     CounterMetric(const std::string& name) : Metric(name), value_(0) {}
 
-    void record(double value) override { /* Not applicable for counter */ }
-    void record(long long value) override {
+    void record(double value) { /* Not applicable for counter */ }
+    void record(long long value) {
         value_.fetch_add(value, std::memory_order_relaxed);
     }
 
-    std::string getValueAsStringAndReset() override {
+    std::string getValueAsStringAndReset() {
         long long current_value = value_.exchange(0, std::memory_order_relaxed);
         return std::to_string(current_value);
     }
--- metrics/GaugeMetric.h
+++ metrics/GaugeMetric.h
@@ -10,17 +10,17 @@ class GaugeMetric : public Metric {
 public:
     GaugeMetric(const std::string& name) : Metric(name), value_(0.0) {}
 
-    void record(double value) override {
+    void record(double value) {
         std::lock_guard<std::mutex> lock(mtx_);
         value_ = value;
     }
-    void record(long long value) override { /* Not applicable for gauge */ }
+    void record(long long value) { /* Not applicable for gauge */ }
 
-    std::string getValueAsStringAndReset() override {
+    std::string getValueAsStringAndReset() {
         std::lock_guard<std::mutex> lock(mtx_);
         std::stringstream ss;
         ss << std::fixed << std::setprecision(2) << value_;
-        value_ = 0.0; // Reset after reading
+        value_ = 0.0;
         return ss.str();
     }
 
